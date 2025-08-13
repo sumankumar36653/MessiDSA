00c1f6564a0597603f6cf88df0daf87279fdaa4e
Suman Kumar <sumankumar36653@gmail.com>
2025-08-13 18:20:32 +0530

    1.loop within a loop

---
+++ WARM UP!!/6.Double Loop/index.js
@@ -0,0 +1,16 @@
+//loop within a loop
+//1
+
+for(i = 0; i < 5; i++) {
+    for(j = 0; j < 5; j++) {
+        console.log("i = " + i + " " + "j = " + j);
+        
+    }
+}
+for(i = 0; i < 3; i++) {
+    for(j = 0; j < 3; j++) {
+        console.log("i = " + i + " " + "j = " + j);
+        
+    }
+}
+
